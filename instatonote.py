719488e1ea76b0dea7625b2fea4846c3becb9b85
thisispiggy <thisispiggy@gmail.com>
2020-03-17 20:52:28 -0400

    refactored code

--- instatonote.py
+++ instatonote.py
@@ -3,24 +3,10 @@ import os
 import glob
 from jinja2 import Environment, FileSystemLoader
 
-"""saves instagram saved posts"""
+"""Saves instagram saved posts"""
 
-USER = input("\nEnter Instagram username: ")
-
-file_loader = FileSystemLoader('templates')
-env = Environment(loader=file_loader)
-
-template = env.get_template('template.html')
-
-L = instaloader.Instaloader(dirname_pattern='posts', save_metadata=False, 
-                            compress_json=False, download_comments=False, 
-                            post_metadata_txt_pattern='', filename_pattern='{shortcode}/{shortcode}',
-                            download_geotags=False)
-L.interactive_login(USER) 
-profile = instaloader.Profile.from_username(L.context, USER)
 
 class Instasaved:
-
     def __init__(self, caption, shortcode):
         try:
             self.title = caption[:50]
@@ -33,17 +19,33 @@ class Instasaved:
         self.url_text = self.url.split("/")[2]
         self.image_url = f'{shortcode}.jpg'
 
-def make_dirs():
 
-    '''makes html directory'''
+def downloadSettings(username):
+    setting = instaloader.Instaloader(dirname_pattern='posts', save_metadata=False,
+                                      compress_json=False, download_comments=False,
+                                      post_metadata_txt_pattern='', filename_pattern='{shortcode}/{shortcode}',
+                                      download_geotags=False)
+    setting.interactive_login(username)
+    return setting
 
-    if not os.path.exists("posts"):
-        os.makedirs("posts")
+
+def getProfile(username, setting):
+    profile = instaloader.Profile.from_username(setting.context, username)
+    return profile
+
+
+def download_saved(profile, setting):
+    for post in profile.get_saved_posts():
+        setting.download_post(post, target=None)
+
+
+def make_dirs(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
 
 def find_images(shortcode):
-    
     '''finds images files in directory'''
-
     path = os.path.join('**', shortcode, '*.jpg')
     image_list = glob.glob(path)
     jpg_images = []
@@ -53,8 +55,8 @@ def find_images(shortcode):
         jpg_images.append(imgurl)
     return jpg_images
 
-def find_videos(shortcode):
 
+def find_videos(shortcode):
     path = os.path.join('**', shortcode, '*.mp4')
     vid_list = glob.glob(path)
     mp4_videos = []
@@ -64,41 +66,54 @@ def find_videos(shortcode):
         mp4_videos.append(vidurl)
     return mp4_videos
 
-def jinja_output(title, url, url_text, jpg_images, mp4_videos, caption):
-    render = template.render(title = title, url = url, url_text = url_text, images = jpg_images, videos = mp4_videos, caption = caption)
-    return render 
+
+def returnTemplate(template):
+    file_loader = FileSystemLoader('templates')
+    env = Environment(loader=file_loader)
+
+    template = env.get_template('template.html')
+    return template
+
+
+def jinja_output(title, url, url_text, jpg_images, mp4_videos, caption, template):
+    render = template.render(title=title, url=url, url_text=url_text,
+                             images=jpg_images, videos=mp4_videos, caption=caption)
+    return render
+
 
 def html_file(render, shortcode):
     saved_html = os.path.join('posts', f'{shortcode}.html')
     with open(saved_html, 'w', encoding='utf-8') as f:
         f.write(render)
-    print(f'saving {shortcode}')
+    print(f'Saving {shortcode}')
 
-def download_saved():
-    for post in profile.get_saved_posts():
-        L.download_post(post, target=None)
 
 def move_videos():
     PATH = os.path.join('posts', '**', '*.mp4')
-    print(PATH)
     videos = glob.glob(PATH)
-    print(videos)
     for vid in videos:
         dest = os.path.join('posts', vid.split('\\')[-1])
         os.rename(vid, dest)
         print(f'moving {vid}')
 
+
 def main():
-    make_dirs()
-    download_saved()
+    make_dirs('posts')
+    username = input("\nEnter Instagram username: ")
+    setting = downloadSettings(username)
+    profile = getProfile(username, setting)
+    download_saved(profile, setting)
     for post in profile.get_saved_posts():
         I = Instasaved(post.caption, post.shortcode)
         title, caption, shortcode, url, url_text, image_url = I.title, I.caption, I.shortcode, I.url, I.url_text, I.image_url
         jpg_images = find_images(shortcode)
         mp4_videos = find_videos(shortcode)
-        render = jinja_output(title, url, url_text, jpg_images, mp4_videos, caption)
+        template = returnTemplate('template.html')
+        render = jinja_output(title, url, url_text,
+                              jpg_images, mp4_videos, caption, template)
         html_file(render, shortcode)
     move_videos()
+
 
 if __name__ == "__main__":
     main()
